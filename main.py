9e2e658317242094981f8de93ed63ab298f14256
Nicholas Ramsay <nick@yousponsor.me>
2019-09-19 11:07:15 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys, subprocess
+from taskrunner import Task, TaskRunner
+
+if __name__ == '__main__':
+    taskRunner = TaskRunner(enableHelp=True)
+    taskRunner.AddTasks([
+        Task(['dock-reset', 'dockreset', 'dreset'], 'defaults delete com.apple.dock; killall Dock', 'Reset the dock to default i.e. on bottom with autohide disabled.'),
+        Task(['dock-autohide-delay-0', 'dockquick', 'dquick'], 'defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.5; killall Dock', 'Enable quick dock showing with disabled animations.'),
+        Task(['dock-orientation-left', 'dockleft', 'dleft'],'defaults write com.apple.dock orientation -string left; killall Dock', 'Places the dock on the left side of the screen.'),
+        Task(['dock-orientation-bottom', 'dockbottom', 'dbottom'], 'defaults write com.apple.dock orientation -string bottom; killall Dock', 'Places the dock on the bottom of the screen.'),
+        Task(['dock-autohide-true', 'dockhide', 'dhide'], 'defaults write com.apple.dock autohide -boolean true; killall Dock', 'Enables dock auto hiding'),
+        Task(['dock-autohide-false', 'docknohide', 'dnohide'], 'defaults write com.apple.dock autohide -boolean false; killall Dock', 'Enables dock auto hiding'),
+    ])
+
+    # Run the command line argument task
+    try:
+        taskRunner.RunTask(sys.argv[1])
+    except Exception as e:
+        print(e)
---
+++ taskrunner.py
@@ -0,0 +1,119 @@
+import sys, subprocess
+
+'''
+    --- Custom Task Runner Utilities ---
+'''
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Task:
+    def __init__(self,name=[],script='',description=''):
+        if type(name) == str:
+            self.name = [name]
+        else:
+            self.name = name
+
+        self.script = script
+        self.description = description
+
+class TaskRunner:
+    def __init__(self, enableHelp=False):
+        self.tasks = []
+        self.enableHelp = enableHelp
+
+        # update help if enabled
+        self.enableHelp
+
+    def __updateHelp(self):
+        if self.enableHelp:
+
+            # Generate Output
+            out = ''
+            for task in self.tasks:
+
+                # for each task, add a new line with names and description
+                
+                ''' 
+                    (1) Show the Task Name
+                    - Check if task has multinames and loop through them
+                    - Otherwise print the only option
+                '''
+                if type(task.name) == list:
+                    for i, name in enumerate(task.name):
+                        if i < len(task.name) - 1:
+                            out += bcolors.OKGREEN + name + bcolors.ENDC + ' | '
+                        else:
+                            out += bcolors.OKGREEN + name + bcolors.ENDC
+                else:
+                    out += name
+
+                '''
+                    (2) Show Description
+                '''
+                out += ' --> ' + (task.description, '...')[task.description == '']
+
+                '''
+                    (3) Add new Line
+                '''
+                out += '\n'
+
+            # Update with new output
+            self.RemoveTask('help', False)
+            self.AddTask(Task(['h', 'help'], 'echo \"' + out + '\"'), False)
+
+    def AddTask(self,task, __doUpdateHelp=True):
+        if task != Task() and type(task) == Task:
+            self.tasks.append(task)
+
+            # update help if enabled as new task has been added
+            if __doUpdateHelp: self.__updateHelp()
+        else:
+            raise Exception('AddTask must take a valid Task object.')
+
+    def AddTasks(self, tasks):
+        # add proper error checking
+        for task in tasks:
+            self.AddTask(task)
+
+    def RemoveTask(self, name='', __doUpdateHelp=True):
+        if type(name) == list:
+            raise Exception('name must be of type str not list.')
+
+        for task in self.tasks:
+            if type(task.name) == list:
+                for alias in task.name:
+                    if alias == name:
+                        self.tasks.remove(task)
+                        if __doUpdateHelp: self.__updateHelp()
+                        return
+            elif task.name == name:
+                self.tasks.remove(task)
+                if __doUpdateHelp: self.__updateHelp()
+                return
+        return
+
+
+    def RunTask(self,name):
+        for task in self.tasks:
+            if task.name == name or name in task.name:
+                for path in self.__execute(task.script):
+                    print(path, end="")
+                return
+        else:
+            raise Exception('The script \'' + name + '\' could not be found.')
+
+    def __execute(self,cmd):
+        popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+        for stdout_line in iter(popen.stdout.readline, ""):
+            yield stdout_line 
+        popen.stdout.close()
+        return_code = popen.wait()
+        if return_code:
+            raise subprocess.CalledProcessError(return_code, cmd)
