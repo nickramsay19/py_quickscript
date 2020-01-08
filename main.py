b8ae015b250e73b8003e82cf93148312b9a70fe1
Nicholas Ramsay <nick@yousponsor.me>
2020-01-08 18:42:28 +1100

    added csv file reading to quickly add scripts

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import sys
 from taskrunner import Task, TaskRunner
-
+    
 if __name__ == '__main__':
 
-    defaultTask = Task('default', 'qs version && printf \"\n\" && qs help')
-    versionTask = Task(['v', 'version'], 'echo \"QuickScript Version 1.3\nCreated by Nicholas Ramsay \"', 'Shows the current QuickScript version.')
+    # setup taskrunner
+    taskRunner = TaskRunner(
+        enableHelp = True, 
+        defaultTask = Task('default', 'qs version && printf \"\n\" && qs help'), 
+        versionTask = Task(['v', 'version'], 'echo \"QuickScript Version 1.3\nCreated by Nicholas Ramsay \"', 'Shows the current QuickScript version.')
+    )
 
-    taskRunner = TaskRunner(enableHelp=True, defaultTask=defaultTask, versionTask=versionTask)
-    taskRunner.AddTasks([
-        Task(['dock-reset', 'dockreset', 'dreset'], 'defaults delete com.apple.dock; killall Dock', 'Reset the dock to default i.e. on bottom with autohide disabled.'),
-        Task(['dock-autohide-delay-0', 'dockquick', 'dquick'], 'defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -float 0.5; killall Dock', 'Enable quick dock showing with disabled animations.'),
-        Task(['dock-orientation-left', 'dockleft', 'dleft'],'defaults write com.apple.dock orientation -string left; killall Dock', 'Places the dock on the left side of the screen.'),
-        Task(['dock-orientation-right', 'dockright', 'dright'],'defaults write com.apple.dock orientation -string right; killall Dock', 'Places the dock on the right side of the screen.'),
-        Task(['dock-orientation-bottom', 'dockbottom', 'dbottom'], 'defaults write com.apple.dock orientation -string bottom; killall Dock', 'Places the dock on the bottom of the screen.'),
-        Task(['dock-autohide-true', 'dockhide', 'dhide'], 'defaults write com.apple.dock autohide -boolean true; killall Dock', 'Enables dock auto hiding.'),
-        Task(['dock-autohide-false', 'dockshow', 'dshow'], 'defaults write com.apple.dock autohide -boolean false; killall Dock', 'Enables dock auto hiding.'),
-    ])
+    # get tasks from file and add to taskRunner
+    with open('tasks.csv', 'r') as f:
+        for l in f.readlines(0):
+            segments = l.split(', ')
+            taskRunner.AddTask(Task(segments[0].split(' '), segments[1], segments[2]))
+
 
     # Run the command line argument task
     try:
